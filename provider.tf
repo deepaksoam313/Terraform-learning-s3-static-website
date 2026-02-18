4d1be9a38c2cc01e1956b33f76680555a0fb8a8e
Deepak Thakur <131188328+deepaksoam313@users.noreply.github.com>
2026-02-18 23:26:12 +0530

    Update S3 bucket name for Terraform backend

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "deepak-terraform-state"
+    bucket         = "deepak-tf-state-ap-south-1-2026"
     key            = "static-site/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock"
