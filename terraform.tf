b35346c2b494ee6e83037c366eef34a3167aa03b
Deepak Thakur <131188328+deepaksoam313@users.noreply.github.com>
2026-02-18 21:58:35 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,16 +1,7 @@
 terraform {
-    cloud {
-    organization = "learn-terraform-deepak"
-
-    workspaces {
-      project = "aws-s3-static-website"
-      name = "learn-terraform-aws-get-started"
-    }
-  }
-
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.15.0"
     }
   }
